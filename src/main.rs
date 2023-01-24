8ccb8524f0bfc1eae3cf132bc3b8da41791318b0
Adam Tulejko <a.tulejko@piwik.pro>
2023-01-24 13:06:09 +0100

    work state

---
+++ src/main.rs
@@ -0,0 +1,151 @@
+#[macro_use]
+extern crate log;
+
+use bytes::BytesMut;
+use tokio::{net::TcpListener, sync::Semaphore, io::{AsyncReadExt, AsyncWriteExt}, time::Instant, fs::File};
+use std::{
+    collections::{BTreeMap, HashMap},
+    sync::{Mutex, Arc}, time::Duration,
+};
+use speedy::Writable;
+
+type Key = u64;
+type Db = Arc<Mutex<HashMap<u64, Vec<u8>>>>;
+type Expiration = Instant;
+
+const KEY_EXPIRE_SEC: u64 = 30;
+
+async fn serve() {
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
+    let db2 = db.clone();
+    let db3 = db.clone();
+
+    let expirations: Arc<Mutex<BTreeMap<Expiration, Key>>> =
+        Arc::new(Mutex::new(BTreeMap::new()));
+    let expirations2 = expirations.clone();
+
+    let last_snapshot = Arc::new(Mutex::new(None));
+
+    tokio::spawn(async move {
+        let expirations_local = expirations2.clone();
+        let db_local = db2.clone();
+        loop {
+            tokio::time::sleep(Duration::from_millis(10)).await;
+            let now = Instant::now();
+            loop {
+                let (expiration, key) = match expirations_local.lock().unwrap().iter().next() {
+                    Some((&expiration, &key)) => (expiration, key),
+                    None => break,
+                };
+
+                if expiration > now {
+                    tokio::time::sleep_until(expiration).await;
+                }
+
+                let mut db_guard = db_local.lock().unwrap();
+                db_guard.remove(&key);
+                drop(db_guard);
+                let mut expirations_guard = expirations_local.lock().unwrap();
+                expirations_guard.remove(&expiration);
+                drop(expirations_guard);
+                debug!("Expired key {key}");
+            }
+        }
+    });
+
+    tokio::spawn(async move {
+        let db_local = db3.clone();
+        let last_snapshot_local = last_snapshot.clone();
+        loop {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+
+            let new_snap_filename = format!("snapshot-{}.db", fastrand::u64(..));
+            let mut file = File::create(&new_snap_filename).await.unwrap();
+
+            let data = {
+                let mut db_guard = db_local.lock().unwrap();
+                let keys_count = db_guard.len();
+                info!("Keys count: {keys_count}");
+                db_guard.shrink_to_fit();
+                db_guard.write_to_vec().unwrap()
+            };
+
+            file.write_all(&data).await.unwrap();
+            file.sync_all().await.unwrap();
+
+            let last_snap_fname = last_snapshot_local.lock().unwrap().clone();
+            if let Some(fname) = last_snap_fname {
+                tokio::fs::remove_file(fname).await.unwrap();
+            }
+            *last_snapshot_local.lock().unwrap() = Some(new_snap_filename);
+        }
+    });
+
+    tokio::spawn(async {
+        loop {
+            let dur = Duration::from_millis(10);
+            let now = Instant::now();
+            while now.elapsed() < dur {}
+            tokio::time::sleep(Duration::from_millis(1)).await;
+        }
+    });
+
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    info!("Listening on {}", listener.local_addr().unwrap());
+
+    let semaphore = Arc::new(Semaphore::new(2048));
+
+    loop {
+        let permit = semaphore.clone().acquire_owned().await.unwrap();
+
+        let (mut stream , addr) = listener.accept().await.unwrap();
+        let local_db = db.clone();
+        let expirations2 = expirations.clone();
+        debug!("Accepted client: {addr:?}");
+
+        tokio::spawn(
+            async move {
+                let mut buf = BytesMut::with_capacity(4 * 1024);
+                loop {
+                    let len = stream.read_buf(&mut buf).await.unwrap();
+
+                    if len == 0 {
+                        debug!("Connection closed");
+                        break;
+                    } else {
+                        // if buf.len() < 1069 && buf.len() != 77 {
+                        //     debug!("Frame not complete (buf len {}), continue", buf.len());
+                        //     continue;
+                        // }
+                        debug!("Buf len: {}", buf.len());
+
+                        let key = fastrand::u64(..);
+
+                        debug!("Writing {key}");
+
+                        local_db.lock().unwrap().insert(key, buf.to_vec());
+                        // db_guard.insert(key, buf);
+                        debug!("Keys in db: {}", local_db.lock().unwrap().len());
+                        // drop(db_guard);
+                        let expire = Instant::now() + Duration::from_secs(KEY_EXPIRE_SEC);
+                        expirations2.lock().unwrap().insert(expire, key);
+                        // queue_guard.insert(expire, key);
+                        // drop(queue_guard);
+
+                        stream.write(b"$-1\r\n").await.unwrap();
+                    }
+
+                    buf.clear();
+                }
+                drop(permit);
+            }
+        );
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    serve().await;
+}
